009f09fa91e042aae29830d6a218fa99bf2f34f2
Artem Kasheev <comrade19632@gmail.com>
2022-03-21 12:42:40 +0300

    change domains

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(boilerplate.saasitive.com www.boilerplate.saasitive.com)
+domains=(zaiym-bez-procentov.online www.zaiym-bez-procentov.online)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
