8e85f2516c11df4d11f4ab12d802b00e00750e9e
Artem Kasheev <comrade19632@gmail.com>
2022-04-13 22:41:09 +0300

    check

--- backend/server/apps/telegram_bot/management/commands/telegram_bot.py
+++ backend/server/apps/telegram_bot/management/commands/telegram_bot.py
@@ -10,13 +10,56 @@ class Command(BaseCommand):
     help = "Running telegram bot"
 
     def handle(self, *args, **kwargs):
-        updater = Updater(settings.TELEGRAM_BOT_TOKEN, use_context=True)
+        return
+        # """
+        # This is a echo bot.
+        # It echoes any incoming text messages.
+        # """
 
-        # Get the dispatcher to register handlers
-        dispatcher = updater.dispatcher
-        dispatcher.add_handler(get_menu_conv_handler())
+        # import logging
 
-        # Start the Bot
-        updater.start_polling()
+        # from aiogram import Bot, Dispatcher, executor, types
 
-        updater.idle()
+        # API_TOKEN = '1683215049:AAG7mWcDTpXwBMiJ3UZMdOPC3Zjf6w6poMY'
+
+        # # Configure logging
+        # logging.basicConfig(level=logging.INFO)
+
+        # # Initialize bot and dispatcher
+        # bot = Bot(token=API_TOKEN)
+        # dp = Dispatcher(bot)
+
+
+        # @dp.message_handler(commands=['start', 'help'])
+        # async def send_welcome(message: types.Message):
+        #     """
+        #     This handler will be called when user sends `/start` or `/help` command
+        #     """
+        #     await message.reply("Hi!\nI'm EwchoBot!\nPowered by aiogram.")
+
+
+        # @dp.message_handler(regexp='(^cat[s]?$|puss)')
+        # async def cats(message: types.Message):
+        #     with open('data/cats.jpg', 'rb') as photo:
+        #         '''
+        #         # Old fashioned way:
+        #         await bot.send_photo(
+        #             message.chat.id,
+        #             photo,
+        #             caption='Cats are here 😺',
+        #             reply_to_message_id=message.message_id,
+        #         )
+        #         '''
+
+        #         await message.reply_photo(photo, caption='Cats are here 😺')
+
+
+        # @dp.message_handler()
+        # async def echo(message: types.Message):
+        #     # old style:
+        #     # await bot.send_message(message.chat.id, message.text)
+
+        #     await message.answer(message.text)
+
+
+        # executor.start_polling(dp, skip_updates=True)
