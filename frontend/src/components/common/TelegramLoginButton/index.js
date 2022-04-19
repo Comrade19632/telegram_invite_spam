d9968bb2f5f6182a52c5b8cab2f22c81ffba6063
Damir Akhmadullin <damir@Damirs-MacBook-Air.local>
2022-04-19 14:38:27 +0300

    New frontend

--- frontend/src/components/common/TelegramLoginButton/index.js
+++ frontend/src/components/common/TelegramLoginButton/index.js
@@ -12,16 +12,6 @@ const TelegramLoginButton = ({
   dataAuthUrl,
   lang,
 }) => {
-
-//   {
-//     "id": 1277782669,
-//     "first_name": "Артём",
-//     "username": "comrade19632",
-//     "photo_url": "https://t.me/i/userpic/320/OH0C1pa8sqbOcXPUgYR6sj-UBcK_L30wi6P0IydDABQ.jpg",
-//     "auth_date": 1647982779,
-//     "hash": "1fe826b0de48d7389c4824d879ab75bd9449c228ee279439f24d3b18396cb40"
-// }
-
   const instance = useRef(null)
 
   useEffect(() => {
@@ -30,18 +20,24 @@ const TelegramLoginButton = ({
         dataOnauth: (user) => { dataOnauth(user) },
       }
 
-      const script = document.createElement('script')
-      script.src = 'https://telegram.org/js/telegram-widget.js?14'
-      script.setAttribute('data-telegram-login', botName)
+      const script = 
+document.createElement('script')
+      script.src = 
+'https://telegram.org/js/telegram-widget.js?14'
+      script.setAttribute('data-telegram-login', 
+botName)
       script.setAttribute('data-size', buttonSize)
       if (cornerRadius !== undefined) {
-        script.setAttribute('data-radius', cornerRadius)
+        script.setAttribute('data-radius', 
+cornerRadius)
       }
-      script.setAttribute('data-request-access', requestAccess)
+      script.setAttribute('data-request-access', 
+requestAccess)
       script.setAttribute('data-userpic', usePic)
       script.setAttribute('data-lang', lang)
       if (dataAuthUrl !== undefined) {
-        script.setAttribute('data-auth-url', dataAuthUrl)
+        script.setAttribute('data-auth-url', 
+dataAuthUrl)
       } else {
         script.setAttribute(
           'data-onauth',
@@ -52,7 +48,16 @@ const TelegramLoginButton = ({
 
       instance.current.appendChild(script)
     }
-  }, [instance])
+  }, [
+    botName,
+    buttonSize,
+    cornerRadius,
+    requestAccess,
+    dataOnauth,
+    dataAuthUrl,
+    lang,
+    usePic,
+  ])
 
   return (
     <div ref={instance} className={className} />
@@ -64,7 +69,8 @@ TelegramLoginButton.propTypes = {
   className: PropTypes.string,
   dataOnauth: PropTypes.func,
   dataAuthUrl: PropTypes.func,
-  buttonSize: PropTypes.oneOf(['large', 'medium', 'small']),
+  buttonSize: PropTypes.oneOf(['large', 'medium', 
+'small']),
   cornerRadius: PropTypes.number,
   requestAccess: PropTypes.string,
   usePic: PropTypes.bool,
@@ -72,12 +78,14 @@ TelegramLoginButton.propTypes = {
 }
 
 TelegramLoginButton.defaultProps = {
-  botName: 'invite_spam_bot',
   buttonSize: 'large',
   dataOnauth: () => undefined,
   lang: 'ru',
   requestAccess: 'write',
   usePic: true,
+  cornerRadius: 0,
+  dataAuthUrl: '#',
+  className: '',
 }
 
 export default TelegramLoginButton
