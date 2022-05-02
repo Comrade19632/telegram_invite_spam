b12bc2f951b74218c76193b153eacf441a8b9ee7
Артур Давидов <art@mac.local>
2022-05-02 19:04:51 +0300

    fix spam.py

--- backend/server/apps/orders/services/spam.py
+++ backend/server/apps/orders/services/spam.py
@@ -11,11 +11,15 @@ import traceback
 from django.conf import settings
 
 from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.types import InputPeerUser
 from telethon.errors.rpcerrorlist import (
     PeerFloodError,
     FloodWaitError, 
     InputUserDeactivatedError,
+    UserDeactivatedBanError,
+    UserIdInvalidError,
+    UserBannedInChannelError,
 )
 
 from apps.orders.services.pars import pars
@@ -43,6 +47,8 @@ def spam(order):
         print("you dont have any active accounts")
         order.in_progress = False
         order.save()
+        order.telethon_accounts.update(is_busy=False)
+
         if order.user:
             send_message_to_user.delay(
                 settings.TELEGRAM_MANUAL_BOT_TOKEN,
@@ -70,7 +76,7 @@ def spam(order):
     order.telethon_accounts.add(account)
     account.is_busy = True
     account.save()
-    
+
     api_id = account.api_id
     api_hash = account.api_hash
     phone_number = account.phone_number
@@ -97,6 +103,69 @@ def spam(order):
 
     print(f"Start with {phone_number} account")
 
+    try:
+        chat = client.get_entity(order.donor_chat_link)
+        client(JoinChannelRequest(chat))
+
+    except ValueError:
+        client.disconnect()
+
+        print("Недействительная ссылка на донор группу")
+
+        account.is_busy = False
+        account.save()
+
+        order.in_progress = False
+        order.save()
+        order.telethon_accounts.update(is_busy=False)
+
+        if order.user:
+            send_message_to_user.delay(
+                settings.TELEGRAM_MANUAL_BOT_TOKEN,
+                order.user.telegram_id,
+                f"Недействительная ссылка на донор группу, заказ завершён",
+            )
+        return
+
+    except UserDeactivatedBanError:
+        client.disconnect()
+        account.is_active = False
+        account.is_busy = False
+        account.date_of_last_deactivate = datetime.datetime.now()
+        account.reason_of_last_deactivate = "Аккаунт был забанен навсегда"
+        account.save()
+        print(re + "[!] Account can`t write in this chat")
+        if order.user:
+            send_message_to_user.delay(
+                settings.TELEGRAM_MANUAL_BOT_TOKEN,
+                order.user.telegram_id,
+                f"Аккаунт {phone_number} забанен навсегда, перезапускаем рассылку на другом аккаунте",
+            )
+        return spam(order)
+
+    except:
+        traceback.print_exc()
+        client.disconnect()
+        account.is_busy = False
+        account.is_active = False
+        account.date_of_last_deactivate = datetime.datetime.now()
+        account.reason_of_last_deactivate = (
+            "Произошла непредвиденная ошибка при рассылке"
+        )
+        account.save()
+
+        order.in_progress = False
+        order.save()
+        order.telethon_accounts.update(is_busy=False)
+
+        if order.user:
+            send_message_to_user.delay(
+                settings.TELEGRAM_MANUAL_BOT_TOKEN,
+                order.user.telegram_id,
+                f"Произошла непредвиденная ошибка при рассылке, заказ завершён",
+            )
+        return
+
     if order.user:
       send_message_to_user.delay(
           settings.TELEGRAM_MANUAL_BOT_TOKEN,
@@ -117,6 +186,19 @@ def spam(order):
             users.append(user)
     
     for user in users:
+        order.refresh_from_db()
+        if not order.in_progress:
+            print(re + "[+] Order has stopped")
+            client.disconnect()
+            account.is_busy = False
+            account.save()
+            if order.user:
+                send_message_to_user.delay(
+                    settings.TELEGRAM_MANUAL_BOT_TOKEN,
+                    order.user.telegram_id,
+                    "Заказ завершён",
+                )
+
         if str(user["id"]) in order.affected_users:
             print(gr + "[+] This user already has been affected")
             continue
@@ -125,13 +207,11 @@ def spam(order):
         
         SLEEP_TIME = random.randint(9, 24)
         receiver = InputPeerUser(user['id'],user['access_hash'])
-
         try:
             print(gr+"[+] Sending Message to:", user['name'])
             client.send_message(receiver, order.spam_message)
             print(gr+"[+] Waiting {} seconds".format(SLEEP_TIME))
             time.sleep(SLEEP_TIME)
-
         except FloodWaitError:
             client.disconnect()
             account.is_active = False
@@ -151,7 +231,6 @@ def spam(order):
                 )
             spam(order)
             return
-
         except PeerFloodError:
             client.disconnect()
             account.is_active = False
@@ -169,15 +248,70 @@ def spam(order):
                     order.user.telegram_id,
                     f"Аккаунт {phone_number} временно заблокирован, перезапускаем рассылку на другом аккаунте",
                 )
-                spam(order)
-                return
-
+            spam(order)
+            return
         except InputUserDeactivatedError:
             print(
                 re
                 + f"The specified user {user['name']} was deleted"
             )
             continue
+        except UserIdInvalidError:
+            print(re + "[!] Invalid object ID for a user. Skipping")
+            continue
+        except UserBannedInChannelError:
+            client.disconnect()
+            account.is_active = False
+            account.is_busy = False
+            account.date_of_last_deactivate = datetime.datetime.now()
+            account.reason_of_last_deactivate = (
+                "Аккаунт был отключен, потому как он забанен в данном канале"
+            )
+            account.save()
+            print(re + "[!] Account can`t write in this chat")
+            if order.user:
+                send_message_to_user.delay(
+                    settings.TELEGRAM_MANUAL_BOT_TOKEN,
+                    order.user.telegram_id,
+                    f"Аккаунт {phone_number} забанен в данном канале, перезапускаем рассылку на другом аккаунте",
+                )
+            spam(order)
+            return
+        except UserDeactivatedBanError:
+            client.disconnect()
+            account.is_active = False
+            account.is_busy = False
+            account.date_of_last_deactivate = datetime.datetime.now()
+            account.reason_of_last_deactivate = "Аккаунт был забанен навсегда"
+            account.save()
+            print(re + "[!] Account can`t write in this chat")
+            if order.user:
+                send_message_to_user.delay(
+                    settings.TELEGRAM_MANUAL_BOT_TOKEN,
+                    order.user.telegram_id,
+                    f"Аккаунт {phone_number} забанен навсегда, перезапускаем рассылку на другом аккаунте",
+                )
+            spam(order)
+            return
+        except:
+            client.disconnect()
+            account.is_active = False
+            account.is_busy = False
+            account.date_of_last_deactivate = datetime.datetime.now()
+            account.reason_of_last_deactivate = (
+                "Аккаунт был отключен по неизвестной причине"
+            )
+            account.save()
+            traceback.print_exc()
+            print(re + "[!] Unexpected Error")
+            if order.user:
+                send_message_to_user.delay(
+                    settings.TELEGRAM_MANUAL_BOT_TOKEN,
+                    order.user.telegram_id,
+                    f"Аккаунт {phone_number} был отключен по неизвестной причине, перезапускаем инвайт на другом аккаунте",
+                )
+            spam(order)
+            return
 
     client.disconnect()
 
